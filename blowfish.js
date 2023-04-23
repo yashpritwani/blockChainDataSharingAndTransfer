afcde29c1479461c26e4aa4a1a01d86dd51b8c5a
yashpritwani <ypritwani@gmail.com>
2023-04-24 03:04:31 +0530

    Modified and tested AES and blowfish functionalities

--- blowfish.js
+++ blowfish.js
@@ -1,13 +1,14 @@
 const crypto = require('crypto');
 const fs = require('fs');
-const ecnryption_method = 'aes-256-cbc';
+const ecnryption_method = 'bf-cbc';
 
 // Encrypt data
 async function encryptData(fileName) {
   let data = fs.readFileSync(`${fileName}.txt`);
-  let random = crypto.randomBytes(16);
+  let random = crypto.randomBytes(8);
   let id = random.toString('hex');
   let hash = await generateIv(id);
+  console.log(id, id.length, hash, hash.length);
   const cipher = crypto.createCipheriv(ecnryption_method, id, hash)
   let encrypted = Buffer.from(cipher.update(data, 'utf8', 'hex') + cipher.final('hex'));
   return fs.writeFile(`${id}.txt`, encrypted, (err) => {
@@ -37,8 +38,8 @@ async function decryptData(id) {
 // }
 
 async function generateIv(id) {
-  return crypto.createHash('sha512').update(id).digest('hex').substring(0, 16);
+  return crypto.createHash('sha512').update(id).digest('hex').substring(0, 8);
 }
 
 // encryptData("hye");
-// decryptData("ab41390b6a0767c4f3016ffe3a88a3a2");
+decryptData("bde48399242b993e");

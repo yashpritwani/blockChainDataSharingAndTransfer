a660b966d77377553ec1a84f3be89e79ddf2ac9a
yash pritwani <yprit@NB297.datatrak.ws>
2023-05-06 12:25:03 +0530

    DES implementation

---
+++ des.js
@@ -0,0 +1,45 @@
+const crypto = require('crypto');
+const fs = require('fs');
+const ecnryption_method = 'des';
+
+// Encrypt data
+async function encryptData(fileName) {
+  let data = fs.readFileSync(`${fileName}.txt`);
+  let random = crypto.randomBytes(16);
+  let id = random.toString('hex');
+  let hash = await generateIv(id);
+  console.log(id, id.length, hash, hash.length);
+  const cipher = crypto.createCipheriv(ecnryption_method, id, hash)
+  let encrypted = Buffer.from(cipher.update(data, 'utf8', 'hex') + cipher.final('hex'));
+  return fs.writeFile(`${id}.txt`, encrypted, (err) => {
+    if (err)
+      console.log(err);
+    else {
+      console.log("File written successfully\n");
+      console.log({ key: id });
+      return { key: id };
+    }
+  });
+}
+
+// Decrypt data
+async function decryptData(id) {
+  let encryptedData = fs.readFileSync(`${id}.txt`);
+  const buff = Buffer.from(encryptedData, 'base64');
+  let hash = await generateIv(id);
+  const decipher = crypto.createDecipheriv(ecnryption_method, id, hash)
+  let decryptedData = decipher.update(buff.toString('utf8'), 'hex', 'utf8') +  decipher.final('utf8');
+  console.log(decryptedData);
+  return decryptedData;
+}
+
+// async function generatekey(id) {
+//   return crypto.createHash('sha512').update(id).digest('hex').substring(0, 32);
+// }
+
+async function generateIv(id) {
+  return crypto.createHash('sha512').update(id).digest('hex').substring(0, 8);
+}
+
+// encryptData("hye");
+decryptData("bde48399242b993e");
